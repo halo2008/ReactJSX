64b39a2e6ab6a7fbc4636f06dd3c71f4682d6def
Konrad Sedkowski <ksedkowski@gmail.com>
2019-01-09 22:01:18 +0100

    fully working input add new user

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,17 +1,29 @@
 class ContactForm extends React.Component{
 	constructor(props) {
 		super(props);
+		this.state = {
+      firstName: '',
+      lastName: '',
+			email: ''
+    };
+		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+
 	}
+
+	handleChange(event){
+		event.preventDefault();
+		this.setState({ [event.target.name]: event.target.value })
+	}
+
 	handleSubmit(event) {
-		event.preventDefault()
+		event.preventDefault();
 		const firstName = event.target.firstName.value
 		const lastName = event.target.lastName.value
 		const email = event.target.email.value
-		console.log(name)
-		if(firstName.trim() !== '' && lastName.trim() !== '' && email.trim() !=='' ){
+			if(firstName.trim() !== '' && lastName.trim() !== '' && email.trim() !=='' ){
 			this.props.addContact(firstName, lastName, email)
-		}		
+		}
 	}
 
 	render() {
@@ -21,24 +33,24 @@ class ContactForm extends React.Component{
 					name = 'firstName'
 					type='text'
 					placeholder='imię'
-					value={this.props.contact.fristName}
-					onChange={this.handleSubmit}
+					value={this.props.contact.fristName, this.state.firstName.value}
+					onChange={this.handleChange}
 					/>
 
 				<input
 					name= 'lastName'
 					type='text'
 					placeholder='Nazwisko'
-					value={this.props.contact.lastName}
-					onChange={this.handleSubmit}
+					value={this.props.contact.lastName, this.state.lastName.value}
+					onChange={this.handleChange}
 					/>
 
 				<input
 					name ='email'
 					type='email'
 					placeholder='Email'
-					value={this.props.contact.email}
-					onChange={this.handleSubmit}
+					value={this.props.contact.email, this.state.email.value}
+					onChange={this.handleChange}
 					/>
 
 				<button type='submit'>Dodaj Kontakt</button>
